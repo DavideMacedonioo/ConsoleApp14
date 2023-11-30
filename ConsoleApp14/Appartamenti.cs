4d579656e55730e06604d2abe3afd02704f2b3b1
francesco <francesco@CICO>
2023-11-30 19:35:40 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp14/Appartamenti.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Appartamenti : Immobili
+    {
+        public int Vani { get; set; }
+        public int Bagni { get; set; }
+    }
+}
---
+++ ConsoleApp14/Immobili.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Immobili
+    {
+        public string Codice { get; set; }
+        public string Indirizzo { get; set; }
+        public string Cap { get; set; }
+        public string Città { get; set; }
+        public int Mq { get; set; }
+
+    }
+}
